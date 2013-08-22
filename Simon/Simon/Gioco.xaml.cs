9220796c6d05b05db0491e8e77b943359286f0d8
Andrea Gottardi <andreagot16@gmail.com>
2013-08-22 19:06:31 +0200

    Now buttons dim

--- Simon/Simon/Gioco.xaml.cs
+++ Simon/Simon/Gioco.xaml.cs
@@ -7,8 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
-using System.Threading;
-
+using System.Windows.Threading;
 namespace Simon
 {
 
@@ -27,33 +26,72 @@ namespace Simon
             SuonoBottone4.Visibility = System.Windows.Visibility.Collapsed;
         }
 
+         void DiventaOpaco1(object sender, EventArgs e)
+        {
+            DispatcherTimer dt = (DispatcherTimer)sender;
+            Button1.Opacity = 1;
+        }
+
+         void DiventaOpaco2(object sender, EventArgs e)
+         {
+             DispatcherTimer dt = (DispatcherTimer)sender;
+             Button2.Opacity = 1;
+         }
+
+         void DiventaOpaco3(object sender, EventArgs e)
+         {
+             DispatcherTimer dt = (DispatcherTimer)sender;
+             Button3.Opacity = 1;
+         }
+
+         void DiventaOpaco4(object sender, EventArgs e)
+         {
+             DispatcherTimer dt = (DispatcherTimer)sender;
+             Button4.Opacity = 1;
+         }
+
         private void Bottone1(object sender, RoutedEventArgs e)
         {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 300);
+            timer.Tick += new EventHandler(DiventaOpaco1);
+
+            Button1.Opacity = opac;
             SuonoBottone1.Play();
+            timer.Start();
         }
 
         private void Bottone2(object sender, RoutedEventArgs e)
         {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 300);
+            timer.Tick += new EventHandler(DiventaOpaco2);
+            
             Button2.Opacity = opac;
             SuonoBottone2.Play();
-            Thread.Sleep(200);
-           // Button2.Opacity = 1;
+            timer.Start();
         }
 
         private void Bottone3(object sender, RoutedEventArgs e)
         {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 300);
+            timer.Tick += new EventHandler(DiventaOpaco3);
+
             Button3.Opacity = opac;
             SuonoBottone3.Play();
-            Thread.Sleep(200);
-           // Button3.Opacity = 1;
+            timer.Start();
         }
 
         private void Bottone4(object sender, RoutedEventArgs e)
         {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 300);
+            timer.Tick += new EventHandler(DiventaOpaco4);
+
             Button4.Opacity = opac;
             SuonoBottone4.Play();
-            Thread.Sleep(200);
-            Button4.Opacity = 1;
+            timer.Start();
         }
 
 
