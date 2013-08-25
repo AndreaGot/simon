377f016ff1af4e896e87c897043eec06f59671e7
Andrea Gottardi <andreagot16@gmail.com>
2013-08-26 00:06:50 +0200

    Async/Await needed

--- Simon/Simon/Gioco.xaml.cs
+++ Simon/Simon/Gioco.xaml.cs
@@ -14,39 +14,45 @@ namespace Simon
     
     public partial class Gioco : PhoneApplicationPage
     {
-
+        int[] tasti = new int[100];
+        int sequenza = 0;
+        int livello = 1;
         double opac = 0.3;
+
+
+
         public Gioco()
         {
+            Random tst = new Random();
             InitializeComponent();
 
             SuonoBottone1.Visibility = System.Windows.Visibility.Collapsed;
             SuonoBottone2.Visibility = System.Windows.Visibility.Collapsed;
             SuonoBottone3.Visibility = System.Windows.Visibility.Collapsed;
             SuonoBottone4.Visibility = System.Windows.Visibility.Collapsed;
+
+            for(int i=0;i<100;i++)
+            {
+                tasti[i] = tst.Next(1, 4);
+            }
+
+            IniziaGioco();
         }
 
          void DiventaOpaco1(object sender, EventArgs e)
         {
-            DispatcherTimer dt = (DispatcherTimer)sender;
             Button1.Opacity = 1;
         }
-
          void DiventaOpaco2(object sender, EventArgs e)
          {
-             DispatcherTimer dt = (DispatcherTimer)sender;
              Button2.Opacity = 1;
          }
-
          void DiventaOpaco3(object sender, EventArgs e)
          {
-             DispatcherTimer dt = (DispatcherTimer)sender;
              Button3.Opacity = 1;
          }
-
          void DiventaOpaco4(object sender, EventArgs e)
          {
-             DispatcherTimer dt = (DispatcherTimer)sender;
              Button4.Opacity = 1;
          }
 
@@ -59,6 +65,24 @@ namespace Simon
             Button1.Opacity = opac;
             SuonoBottone1.Play();
             timer.Start();
+
+
+            sequenza++;
+            if (ControllaGiusto(1))
+            {
+                if (sequenza == livello)
+                {
+                    livello++;
+                    sequenza = 0;
+                    Titolo.Text = "Livello " + livello;
+                    IniziaGioco();
+                }
+            }
+            else
+            {
+                NavigationService.Navigate(new Uri("/FineGioco.xaml", UriKind.Relative));
+            }
+
         }
 
         private void Bottone2(object sender, RoutedEventArgs e)
@@ -70,6 +94,24 @@ namespace Simon
             Button2.Opacity = opac;
             SuonoBottone2.Play();
             timer.Start();
+
+
+            sequenza++;
+            if (ControllaGiusto(2))
+            {
+                if (sequenza == livello)
+                {
+                    livello++;
+                    sequenza = 0;
+                    Titolo.Text = "Livello " + livello;
+                    IniziaGioco();
+                }
+            }
+            else
+            {
+                NavigationService.Navigate(new Uri("/FineGioco.xaml", UriKind.Relative));
+            }
+
         }
 
         private void Bottone3(object sender, RoutedEventArgs e)
@@ -79,8 +121,27 @@ namespace Simon
             timer.Tick += new EventHandler(DiventaOpaco3);
 
             Button3.Opacity = opac;
+            Titolo.Text = "Livello " + livello;
             SuonoBottone3.Play();
             timer.Start();
+
+
+            sequenza++;
+            if (ControllaGiusto(3))
+            {
+                if (sequenza == livello)
+                {
+                    livello++;
+                    sequenza = 0;
+                    Titolo.Text = "Livello " + livello;
+                    IniziaGioco();
+                }
+            }
+            else
+            {
+                NavigationService.Navigate(new Uri("/FineGioco.xaml", UriKind.Relative));
+            }
+
         }
 
         private void Bottone4(object sender, RoutedEventArgs e)
@@ -92,10 +153,86 @@ namespace Simon
             Button4.Opacity = opac;
             SuonoBottone4.Play();
             timer.Start();
+
+            sequenza++;
+            if (ControllaGiusto(4))
+            {
+                if (sequenza == livello)
+                {
+                    livello++;
+                    sequenza = 0;
+                    Titolo.Text = "Livello " + livello;
+                    IniziaGioco();
+                }
+            }
+            else
+            {
+                NavigationService.Navigate(new Uri("/FineGioco.xaml", UriKind.Relative));
+            }
+            
         }
 
 
+        async IniziaGioco()
+        {
+            DispatcherTimer timer5 = new DispatcherTimer();
+            timer5.Interval = new TimeSpan(0, 0, 0, 3);
+            timer5.Start();
+            for (int i = 0; i < livello; i++)
+            {
+                switch (tasti[i])
+                {
+                    case 1:
+                        DispatcherTimer timer = new DispatcherTimer();
+                        timer.Interval = new TimeSpan(0, 0, 0, 0, 300);
+                        timer.Tick += new EventHandler(DiventaOpaco1);
+                        Button1.Opacity = opac;
+                        SuonoBottone1.Play();
+                        timer.Start();
+                        //timer5.Start();
+                        break;
+                    case 2:
+                        DispatcherTimer timer2 = new DispatcherTimer();
+                        timer2.Interval = new TimeSpan(0, 0, 0, 0, 300);
+                        timer2.Tick += new EventHandler(DiventaOpaco2);
+                        Button2.Opacity = opac;
+                        SuonoBottone2.Play();
+                        timer2.Start();
+                        //timer5.Start();
+                        break;
+                    case 3:
+                        DispatcherTimer timer3 = new DispatcherTimer();
+                        timer3.Interval = new TimeSpan(0, 0, 0, 0, 300);
+                        timer3.Tick += new EventHandler(DiventaOpaco3);
+                        Button3.Opacity = opac;
+                        SuonoBottone3.Play();
+                        timer3.Start();
+                        //timer5.Start();
+                        break;
+                    case 4:
+                        DispatcherTimer timer4 = new DispatcherTimer();
+                        timer4.Interval = new TimeSpan(0, 0, 0, 0, 300);
+                        timer4.Tick += new EventHandler(DiventaOpaco4);
+                        Button4.Opacity = opac;
+                        SuonoBottone4.Play();
+                        timer4.Start();
+                       
+                        break;
+                }
+                await Task.Delay(1000);
+            }
 
+        }
+        bool ControllaGiusto(int x)
+        {
+            if (tasti[sequenza - 1] != x)
+                return false;
+            else
+                return true;
+        }
 
+        void PerdiTempo(object sender, EventArgs e) { Button2.Opacity = 1; }
+
+ 
     }
 }
